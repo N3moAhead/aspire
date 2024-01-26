925025da023c20523bdeadb64845640210471dcb
Lukas Heberling <lukaselia.heberling@gmail.com>
2024-01-27 00:36:09 +0100

    ➕ added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.body);
 root.render(<h2>First Render of Aspire</h2>);
